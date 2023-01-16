1f3c5026e7a2772cd604f6b28164fde25f3843e3
AlexandruSchneider <alexandru.schneider@stud.fhgr.ch>
2023-01-16 12:01:20 +0100

    Optimized code layout

--- v3/project/preprocessing/Preprocessing.py
+++ v3/project/preprocessing/Preprocessing.py
@@ -20,8 +20,11 @@ from .imputation import imput_missing_values
 def preprocess(df):
     print(f'-------------------------------------\nStarting Preprocessing\n-------------------------------------')
     # job_id is not needed
-    df.drop(columns=['job_id'], inplace=True)
+    # define text features to remove
+    text_features = ['title', 'company_profile', 'description', 'requirements', 'benefits']
     drop_lines = [8850, 17233, 13541]
+    # drop job_id and unnecessary lines
+    df.drop(columns=['job_id'], inplace=True)
     df.drop(drop_lines, inplace=True)
     # filna 'Unspecified' for location (country, state, city)
     df.location.fillna('Unspecified, Unspecified, Unspecified', inplace=True)
@@ -35,16 +38,13 @@ def preprocess(df):
     # y_train has to be passed, as there are additional rows generated if there are multiple cities given
     X_train, y_train = process_location(X_train, y_train)
     X_train = process_salary_range(X_train)
-
-    # define text features to remove
-    text_features = ['title', 'company_profile', 'description', 'requirements', 'benefits']
     X_train.drop(columns=text_features, inplace=True)
-    
     X_train = imput_missing_values(X_train)
     
     X_test, y_test = process_location(X_test, y_test)
     X_test = process_salary_range(X_test)
     X_test.drop(columns=text_features, inplace=True)
     X_test = imput_missing_values(X_test)
+    
     print(f'-------------------------------------\nEnd Preprocessing\n-------------------------------------')
     return X_train, X_test, y_train, y_test
